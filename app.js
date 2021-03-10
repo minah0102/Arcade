c122019dee58d8463bdbd523f4cfe0505c1d1fc1
minah0102 <minah0102@gmail.com>
2021-03-09 21:25:05 -0500

    updated commit

--- app.js
+++ app.js
@@ -1,71 +1,88 @@
-const mole = $("<div class='mole'></div>");
-const gameZone = $("#game-zone");
-const startButton = $(".start-button");
-let score = 0;
-let gameOver = false;
-
-const moleGrid = [
-  ["e", "e", "e"],
-  ["e", "e", "e"],
-  ["e", "e", "e"],
-];
-
+let gameZone = $("#game-zone");
 function createMoleGridCell() {
   for (i = 0; i < 9; i++) {
     gameZone.append(`<div class="cell"></div>`);
   }
 }
-createMoleGridCell();
+const mole = $('<img src="mole_cartoon.png" class="mole">');
+const fasterMole = $('<img src="faster_mole.png" class="fasterMole">');
+const civilianMole = $('<img src="civilian_mole.png" class="civilian-mole">');
 
 function renderRandomMoles() {
-  const math = Math.floor(Math.random() * 9);
-  $(".cell").empty();
+  gameZone.empty();
+  createMoleGridCell();
 
-  setInterval(() => {
-    
+  let appearRandomMoles = setInterval(() => {
     $(".cell")
       .eq(Math.floor(Math.random() * 9))
       .append(mole);
 
-    mole.on("click", function () {
-      //debugger
-      mole.remove();
-      score++;
-      $(".score-points").text(`Score: ${score}`);
-
-      // function scoring() {
-      //   if (
-      //     mole.click(function () {
-      //       mole.remove();
-      //     })
-      //   ) {
-      //     score++.click(mole);
-      //     $(".score-points").text(`Score: ${score}`);
-      //   } else {
-      //     $('.score-points').stop();
-      //     //score no update
-      //   }
-      // }
-      // scoring();
-    });
-    
+    mole.show();
   }, 2000);
-  //setTimeout(() => gameOver = true, 10000);
+
+  setTimeout(() => {
+    clearInterval(appearRandomMoles);
+    mole.remove();
+  }, 21000);
+
+  let appearRandomMolesFaster = setTimeout(() => {
+    appearRandomMolesFaster = setInterval(() => {
+      $(".cell")
+        .eq(Math.floor(Math.random() * 9))
+        .append(fasterMole);
+
+      fasterMole.show();
+    }, 1000);
+  }, 22000);
+
+  setTimeout(() => {
+    clearInterval(appearRandomMolesFaster);
+    fasterMole.remove();
+  }, 32000);
+
+  let appearCivilianMoles = setTimeout(() => {
+    appearCivilianMoles = setInterval(() => {
+      $(".cell")
+        .eq(Math.floor(Math.random() * 9))
+        .append(civilianMole);
+
+      civilianMole.show();
+    }, 700);
+  }, 33000);
+
+  setTimeout(() => {
+    gameZone.html('<h3 class="game-over">Game Over</h3>');
+    clearInterval(appearCivilianMoles);
+  }, 50000);
+
+  const math = Math.floor(Math.random() * 9);
+  let scoreBoard = $(".score-points");
+  let score = 0;
+
+  mole.click(function () {
+    score += 10;
+    console.log(`${score}`);
+    scoreBoard.text(`Score: ${score}`);
+    mole.hide();
+  });
+
+  fasterMole.click(function () {
+    score += 20;
+    console.log(`${score}`);
+    scoreBoard.text(`Score: ${score}`);
+    fasterMole.hide();
+  });
+
+  civilianMole.click(function () {
+    score += 50;
+    console.log(`${score}`);
+    scoreBoard.text(`Score: ${score}`);
+    civilianMole.hide();
+  });
 }
 
-function startGame() {
-  startButton.click(() => renderRandomMoles());
-}
-startGame();
-
-//call setInterval in global scope
-
-//create mole cell - el in a row
-//have div
-
-//renderGrid() - clear out grid, loop over moleGrid - for each el of mole grid
-//append that into the grid - call these in set interval
-
-//randomize the mole appeared in moleGrid
-
-//click mole - score points
+const startButton = $(".start-button");
+startButton.click(function () {
+  renderRandomMoles();
+  $(".score-points").text(`Score: 0`);
+});
